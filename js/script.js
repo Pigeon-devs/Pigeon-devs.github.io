f1a204c7b3da10853fea89160aa64d268e856d09
Pigeon <274677580+Pigeon-devs@users.noreply.github.com>
2026-04-15 10:29:24 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const lang = document.documentElement.lang;
+
+
 /* =========================
    HERO SLIDESHOW (FIXED)
    ========================= */
@@ -36,7 +39,11 @@ document.querySelector(".hero").addEventListener("click", stopSlideshow);
 let allStories = [];
 
 async function loadStories() {
-    const res = await fetch("stories.json");
+    let json_file = "json/stories_en.json"
+    if (lang === "de") {
+        json_file = "json/stories_de.json"
+    }
+    const res = await fetch(json_file);
     allStories = await res.json();
     renderStories();
 }
@@ -46,21 +53,20 @@ let showAllP = false;
 
 function renderStories() {
     const grid = document.getElementById("storyGrid");
-    console.log(allStories)
 
     grid.innerHTML = "";
 
     const vs = showAllP ? allStories : allStories.slice(0, 10);
 
     vs.forEach(v => {
-        const imgPathP = `images/people/person${v.id}.jpg`;
+        const imgPathP = `images/webp/people/person${v.id}.webp`;
         const storyHtml = v.story.map(line => `${line}`).join("<br>");
 
 
         grid.innerHTML += `
             <div class="story-card">
                 <div class="story-img-wrapper">
-                    <img src="${imgPathP}">
+                    <img src="${imgPathP}" loading="lazy" alt="">
                 </div>
                 <div class="card-body">
                     <h5>${v.name}, ${v.age || "--"}</h5><hr>
@@ -83,7 +89,11 @@ let allVictims = [];
 let showAll = false;
 
 async function loadVictims() {
-    const res = await fetch("victims.json");
+    let json_file = "json/victims_en.json"
+    if (lang === "de") {
+        json_file = "json/victims_de.json"
+    }
+    const res = await fetch(json_file);
     allVictims = await res.json();
     renderVictims();
 }
@@ -92,25 +102,34 @@ function renderVictims() {
     const grid = document.getElementById("victimGrid");
     const btn = document.getElementById("toggleBtn");
 
+    let age_preview = "Age"
+    let show_more_preview = "Show More"
+    let show_less_preview = "Show Less"
+
+    if (lang === "de") {
+        age_preview = "Alter"
+        show_more_preview = "mehr zeigen"
+        show_less_preview = "weniger zeigen"
+    }
+
     grid.innerHTML = "";
 
     const visibleVictims = showAll ? allVictims : allVictims.slice(0, 10);
 
     visibleVictims.forEach(v => {
-        const imgPath = `images/people/p${v.id}.jpg`;
+        const imgPath = `images/webp/people/p${v.id}.webp`;
 
 
         grid.innerHTML += `
     <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4 victim-wrapper">
         <div class="victim-card w-100">
 
-            <img src="${imgPath}"
-                 onerror="this.src='images/fallback.jpg'">
+            <img src="${imgPath}" loading="lazy" alt="">
 
             <div class="victim-text">
                 <div class="victim-name">${v.name}</div>
                 <div class="victim-extra">${v.extra_info || ""}</div>
-                <div class="victim-age">Age: ${v.age || "--"}</div>
+                <div class="victim-age">${age_preview}: ${v.age || "--"}</div>
             </div>
 
         </div>
@@ -125,7 +144,7 @@ function renderVictims() {
         btn.style.display = "none";
     } else {
         btn.style.display = "inline-block";
-        btn.textContent = showAll ? "Show Less" : "Show More";
+        btn.textContent = showAll ? show_less_preview : show_more_preview;
     }
 }
 
